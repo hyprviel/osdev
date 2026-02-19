159328fc3a769de44b8bf10c8dc5a0e5190f13b9
hyprviel <hyprviel@users.noreply.github.com>
2026-02-19 22:42:33 +0700

    chore: delete c files

--- kernel/kernel.c
+++
@@ -1,6 +0,0 @@
-#include <hw/vga.h>
-
-void kernel_main() {
-    vga_init();
-    vga_print(vga_mkattr(VGA_COLOR_BLACK, VGA_COLOR_WHITE), "hello world!");
-}
--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -10,10 +10,8 @@ using vga::Color;
 
 extern "C" void kernel_main() {
     vga::init();
-    // vga::print(vga::Color::Black, vga::Color::White, "Hello, World!");
+
     vga::Writer out(Color::Black, Color::White);
     out << "Hello, world!\n";
-
-    kernel::panic("memory gut fucked up guyz pls help :(");
 }
 
